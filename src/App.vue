8185e50e85510041fad0f4f1b53f995fc412cc28
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-08-14 14:12:38 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Dashboard msg="Hello Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import Dashboard from "./components/Dashboard";
+
+export default {
+  name: "App",
+  components: {
+    Dashboard
+  }
+};
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        width: 100%;
+        height: 100%;
+    }
+    .fade-enter-active,
+    .fade-leave-active {
+        transition-duration: 0.3s;
+        transition-property: height, opacity;
+        transition-timing-function: ease;
+        overflow: hidden;
+    }
+
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0
+    }
+</style>
